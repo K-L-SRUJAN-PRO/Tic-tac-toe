ffe4135aea684072a60f2b3d007369aa136ddf64
K-L-SRUJAN-PRO <klsrujan.1614@gmail.com>
2026-01-22 12:24:31 +0530

    added all logic of screen 1 and screen2

--- app.js
+++ app.js
@@ -1,19 +1,64 @@
-let si=document.querySelector(".si");
-let pa=document.querySelector(".pa");
-let st=document.querySelector(".st");
+let si = document.querySelector(".si");
+let pa = document.querySelector(".pa");
+let st = document.querySelector(".st");
+let el = document.querySelectorAll(".el");
+let screen = document.querySelector(".screen");
+let hh = document.querySelector(".hh");
+
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+let c = 0;
 
 
-st.addEventListener("click",()=>{
-    localStorage.setItem("ele",1);
-    console.log("st");
+el.forEach(events => {
+    events.addEventListener("mouseenter", (event) => {
+        event.target.style.transform = "translateY(-10px)";
+    });
+    events.addEventListener("mouseleave", (event) => {
+        event.target.style.transform = "translateY(0px)";
+    });
+
+    events.addEventListener("click", async() => {
+        c = events.getAttribute("data-value");
+        
+        sho("i2");
+        await fadd();
+    });
+    
+
 });
 
-pa.addEventListener("click",()=>{
-    console.log("pa");
-    localStorage.setItem("ele",2);
-});
+const ids=["i1","i2"];
 
-si.addEventListener("click",()=>{
-    console.log("si");
-    localStorage.setItem("ele",3);
-});
+function sho(id){
+    for(let i of ids){
+        document.getElementById(i).classList.add("hide");   
+    }
+    document.getElementById(id).classList.remove("hide");
+}
+
+
+
+
+
+
+//second page--------------------------------------------------------
+
+async function fadd() {
+
+    const ar = ["Stone", "Paper", "Scissor", "Shoot!!"];
+
+    for (let i of ar) {
+        hh.classList.add("op0");
+
+        await delay(1000);
+        hh.innerText = i;
+        console.log(hh.innerText);
+
+        hh.classList.remove("op0");
+        await delay(1000);
+
+    }
+    // window.location.href='result.html';
+}
+
